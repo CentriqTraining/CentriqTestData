3f00e910a01472553700ec5553dcc9891a26662b
Mike Rissen <Mrissen@centriq.com>
2018-08-16 20:07:47 -0500

    fixed azure change issues

--- CentriqDataSamples/CentriqDataSamples/Models/Toons1.Designer.cs
+++ CentriqDataSamples/CentriqDataSamples/Models/Toons1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\student\documents\visual studio 2013\Projects\AzureCloudService1\WebRole1\Models\Toons.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Documents\GitHub\CentriqTestData\CentriqDataSamples\CentriqDataSamples\Models\Toons.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
